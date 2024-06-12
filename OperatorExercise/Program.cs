cb584c7b22a7c53f918acbcf558a81c26b82483e
“Jessica <osias.jessica@gmail.comgit config --global user.email osias.jessica@gmail.com>
2024-06-12 19:04:49 -0400

    Exercise1 and 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int a = 17;
+            int b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+
+            Console.WriteLine("Please write the radius of the circle:");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area; 
+        }
+        
     }
 }
